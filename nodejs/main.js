f404a98b27ed9dc0812e2da1fca68e3d243727e3
Kingo Wang <wanggenguo@outlook.com>
2016-09-15 14:21:02 +1000

    add yargs

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,3 +1,8 @@
+var argv = require('yargs')
+  .usage("Usage --l=[num] --b=[num]")
+  .demand(['l', 'b'])
+  .argv;
+
 var rect = require('./rectangle');
-console.log(rect().perimeter(1,2));
+console.log(rect().perimeter(argv.l,argv.b));
 console.log("end");
