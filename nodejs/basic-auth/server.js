e42257eeadd2187ecbfabcf9fbf46cf4b4f19379
Kingo Wang <wanggenguo@outlook.com>
2016-10-05 17:45:42 +1100

    basic outh

---
+++ nodejs/basic-auth/server.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(morgan('dev'));
+
+function auth (req, res, next) {
+    console.log(req.headers);
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+        return;
+    }
+
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+    }
+}
+
+app.use(auth);
+
+app.use(bodyParser.json());
+
+app.all('/dishes', function(req,res,next) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      next();
+});
+
+app.get('/dishes', function(req,res,next){
+        res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', function(req, res, next){
+     res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes', function(req, res, next){
+        res.end('Deleting all dishes');
+});
+
+app.get('/dishes/:dishId', function(req,res,next){
+        res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+});
+
+app.put('/dishes/:dishId', function(req, res, next){
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + 
+            ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', function(req, res, next){
+        res.end('Deleting dish: ' + req.params.dishId);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(err,req,res,next) {
+            res.writeHead(err.status || 500, {
+            'WWW-Authenticate': 'Basic',
+            'Content-Type': 'text/plain'
+        });
+        res.end(err.message);
+});
+
+
+app.listen(port, hostname, function(){
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
