22581bc9cc5789c6f976835209e5dc23e9a74b6b
Kingo Wang <genguo@genguo-macbookpro3.roam.corp.google.com>
2020-02-11 11:43:09 -0800

    tm

--- tools/tomato.sh
+++ tools/tomato.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-log_file="/Users/genguo/.wgg/tm.log"
 fmt="+%Y/%m/%d %H:%M:%S"
 mins=$1
 mins=${mins:=30}
+log_file=$2
+log_file=${log_file:="/Users/genguo/.wgg/tm.log"}
 echo "" | tee -a $log_file
 echo -n Start $(date "$fmt") $mins | tee -a $log_file
 echo "" # line break
@@ -10,9 +11,19 @@ while [ $mins -gt 0 ]
 do
   echo $mins left...
   mins=$(( $mins - 1 ))
-  sleep 60
+  read -t 60 OP
+  case $OP in
+    q)
+      break
+      ;;
+    *)
+      ;;
+  esac
 done
-#osascript -e 'display alert "Cong!" message "Take a break now"'
+let "actual = $SECONDS / 60"
+finish_date=$(date "$fmt")
+echo Time Up, Actual Minutes: $actual
 answer=$(osascript -e 'display dialog "Tomato: Time up! How the pomodoro went?" buttons {"Done", "Half", "Failed"} default button "Done"')
 result=${answer#"button returned:"}
-echo -n " Finished" $(date "$fmt") $result | tee -a $log_file
+echo -n " Finished" $finish_date $result $actual | tee -a $log_file
+echo "" # line break
